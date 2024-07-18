2d6f4cf2a76e928d584a31e0a16cf44232632e08
Bharath Horatti <bharath@aveshasystems.com>
2024-07-18 13:02:39 +0530

    fix(): Fixed security vulnerabilities
    Signed-off-by: Bharath Horatti <bharath@aveshasystems.com>

--- pkg/networkservice/common/metrics/metadata.go
+++ pkg/networkservice/common/metrics/metadata.go
@@ -19,13 +19,13 @@ package metrics
 import (
 	"context"
 
-	"go.opentelemetry.io/otel/metric/instrument/syncint64"
+	"go.opentelemetry.io/otel/metric"
 
 	"github.com/networkservicemesh/sdk/pkg/networkservice/utils/metadata"
 )
 
 type keyType struct{}
-type metricsMap = map[string]syncint64.Histogram
+type metricsMap = map[string]metric.Int64Counter
 
 func loadOrStore(ctx context.Context, metrics metricsMap) (value metricsMap, ok bool) {
 	rawValue, ok := metadata.Map(ctx, false).LoadOrStore(keyType{}, metrics)
